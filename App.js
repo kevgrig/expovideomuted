63646dcacfcf4717ec8187aed13a309ebc89f1a3
Kevin <kevin@myplaceonline.com>
2020-12-06 18:06:26 -0800

    Simplify example

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Video } from 'expo-av';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
       <Video
         source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
         rate={1.0}
         volume={1.0}
         isMuted={false}
         resizeMode="cover"
-        shouldPlay
-        isLooping
+        shouldPlay={true}
+        isLooping={true}
         style={{ width: 300, height: 300 }}
+        useNativeControls={true}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
